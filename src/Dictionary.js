c566d6f908efc98f4be91ec0ed4d22e85c9f68a6
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-05-07 11:33:20 +0100

    styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -41,7 +41,7 @@ export default function Dictionary(props) {
         axios.get(apiUrl).then(handleDictionaryResponse);
 
         let pexelsApiKey = "563492ad6f91700001000001cc8f0eb1e319408288af5e3df9618698";
-        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=5`;
         let headers = { "Authorization": `Bearer ${pexelsApiKey}` };
         axios.get(pexelsApiUrl, { headers: headers }).then(handlePhotosResponse);
 
